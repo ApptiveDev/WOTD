bb65edd00d7e693b046c3ea0176ca7e77bc5d467
h-hye <140836353+h-hye@users.noreply.github.com>
2025-06-28 20:31:33 +0900

    Feat: S3 이미지 업로드 및 삭제 기능 구현 (#38)
    # 변경점 👍
    - 이미지 업로드 기능 구현 (S3에 저장)
    - 이미지 삭제 기능 추가 (imageUrl 기반 삭제)
    - `/images/upload` → 이미지 업로드 (POST)
    - `/images/delete` → 이미지 삭제 (DELETE)
    
    # 테스트 💻
    - Postman으로 이미지 업로드 테스트 완료 (form-data: key=image)
    - Postman으로 이미지 삭제 테스트 완료 (Param: key=imageUrl)
    
    ### 테스트 주의 사항 🚨
    - 로컬 환경에서 `application.properties` ↔️ `.env` 파일 간 치환이 되지 않는 경우가 많았습니다!
    - 그러나 보안상 문제로 키 값을 직접 매칭하면 안됩니다!
    - 로컬에서 테스트 하는 경우에는 기존 값을 주석 처리한 후 `.env` 값을 직접 import하는 변수를 추가하여 테스트
    가능합니다!
    - 테스트 후에는 기존 코드로 돌려놓아야 합니당~
    
    # 보안 설정 🔐
    - Spring Security에서 `/**` (일부 경로 허용)에서 (모든 접근 경로 허용)으로 변경 
    - `.env` 파일 변경, 변경 사항은 디스코드 참조
    
    # API 및 스크린샷 📚 
    - `노션 - API 명세서` 참조
    
    ---------
    
    Co-authored-by: 9yub <tbvjzldnl@gmail.com>

---
+++ backend/src/main/java/com/example/apptive_3team/controller/ImageController.java
@@ -0,0 +1,29 @@
+package com.example.apptive_3team.controller;
+
+import com.example.apptive_3team.service.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/images")
+public class ImageController {
+
+    // 이미지 업로드 서비스
+    private final ImageService imageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("image") MultipartFile image) {
+        String imageUrl = imageService.upload(image);
+        return ResponseEntity.ok(imageUrl);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> delete(@RequestParam("imageUrl") String imageUrl) {
+        imageService.delete(imageUrl);
+        return ResponseEntity.ok("삭제 완료!");
+    }
+
+}
