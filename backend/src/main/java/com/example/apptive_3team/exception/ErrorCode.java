33bc45cf9700f352f081eaa30be1b33f117b40e2
9yub <tbvjzldnl@gmail.com>
2025-07-03 18:50:46 +0900

    Feat: 무드 리포트 기능에 필요한 예외처리를 추가

--- backend/src/main/java/com/example/apptive_3team/exception/ErrorCode.java
+++ backend/src/main/java/com/example/apptive_3team/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     NOT_SUPPORTED_LOCATION_ERROR(HttpStatus.BAD_REQUEST, "지원되지 않는 경도, 위도 입니다."),
     NOT_SUPPORTED_DATE_ERROR(HttpStatus.BAD_REQUEST, "예보는 최대 30일까지만 지원합니다."),
     GET_WEATHER_API_ERROR(HttpStatus.BAD_REQUEST, "날씨 데이터를 불러오는 중 오류가 발생했습니다."),
-    INVALID_KAKAO_ACCESS_TOKEN_ERROR(HttpStatus.BAD_REQUEST, "유효하지 않은 카카오 액세스 토큰 입니다.")
+    INVALID_KAKAO_ACCESS_TOKEN_ERROR(HttpStatus.BAD_REQUEST, "유효하지 않은 카카오 액세스 토큰 입니다."),
+    MOOD_REPORT_NOT_FOUND_ERROR(HttpStatus.BAD_REQUEST, "기록된 코디가 없습니다. 무드 리포트를 작성해 주세요!")
     ;
 
     private final HttpStatus status;
