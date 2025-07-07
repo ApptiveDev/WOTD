8a469fec9b14d40fd92cd86742ee9fd5e2e93e52
h-hye <140836353+h-hye@users.noreply.github.com>
2025-07-07 23:42:23 +0900

    Refactor/jwt key (#52)
    jwt 시크릿키 설정 수정했습니다.
    
    카카오 회원가입 부분 로그인 분기 처리 삭제했습니다.
    회원가입 api : kakao/signup
    로그인 api : users/me

--- backend/src/main/java/com/example/apptive_3team/controller/KakaoController.java
+++ backend/src/main/java/com/example/apptive_3team/controller/KakaoController.java
@@ -4,7 +4,6 @@ import com.example.apptive_3team.ApiResponse;
 import com.example.apptive_3team.dto.UserRequestDTO;
 import com.example.apptive_3team.dto.UserResponseDTO;
 import com.example.apptive_3team.entity.User;
-import com.example.apptive_3team.repository.KakaoRepository;
 import com.example.apptive_3team.service.KakaoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,21 +11,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class KakaoController {
 
     private final KakaoService kakaoService;
-    private final KakaoRepository kakaoRepository;
 
-    /**
- * 카카오 로그인 또는 회원가입 처리
- * 요청 DTO로 accessToken과 agree를 받고, 사용자 정보와 JWT 포함 응답 반환
- */
-@PostMapping("/kakao/login")
-public ResponseEntity<?> kakaoLogin(@Valid @RequestBody UserRequestDTO requestDTO) {
-    UserResponseDTO responseDTO = kakaoService.loginOrRegisterWithToken(requestDTO);
-    return ResponseEntity.ok(ApiResponse.success("요청이 정상적으로 수행되었습니다.", responseDTO));
+    @PostMapping("/kakao/signup")
+    public ResponseEntity<?> kakaoLogin(@Valid @RequestBody UserRequestDTO requestDTO) {
+        UserResponseDTO responseDTO = kakaoService.signUp(requestDTO);
+        return ResponseEntity.ok(ApiResponse.success("요청이 정상적으로 수행되었습니다.", responseDTO));
     }
 
     @GetMapping("/users/me")
