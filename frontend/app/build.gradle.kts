9f83873798f3cea4daec572d21d60b82ad0444b1
kimgiyun <gykim_kr@naver.com>
2025-07-07 19:29:55 +0900

    [Chore] KSP 버전 업 - author: 김기윤

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.dagger.hilt.android") version "2.48"
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27"
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
 }
 
 android {
